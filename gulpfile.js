b9638d2ec59088bf39124ebaca1f7a7c36fe9a9f
Sven Hecht <hecht@v22013101860315074.yourvserver.net>
2015-05-07 19:14:46 +0200

    some code

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var gulp = require('gulp');
+var livereload = require('gulp-livereload');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+
+gulp.task('update', function() {
+    gulp.src('views/**/*.handlebars').pipe(livereload());
+});
+
+gulp.task('watch', function() {
+    console.log("started watching...");
+    livereload.listen();
+    gulp.watch('views/**/*.handlebars', ['update']);
+});
+
+
+gulp.task('sass', function () {
+  gulp.src('./public/scss/*.scss')
+//    .pipe(sourcemaps.init())
+    .pipe(sass({ includePaths: require('node-bourbon').includePaths }))//.on('error', sass.logError))
+  //  .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./public/css'));
+});
