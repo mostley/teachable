43f4b28282956e4a499c02d9d1880b03cc63d023
Sven Hecht <sven.hecht@gmail.com>
2015-08-30 00:38:45 +0200

    fixed config

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-    'url' : 'mongodb://mongo/teachable'
-    //'url' : 'mongodb://localhost/teachable'
+    //'url' : 'mongodb://mongo/teachable'
+    'url' : 'mongodb://localhost/teachable'
     //'url' : 'mongodb://teachable:teachable@ds043982.mongolab.com:43982/teachable'
 };
